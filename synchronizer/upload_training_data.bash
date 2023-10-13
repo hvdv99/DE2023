a983088dbb9db3b425e4b8262f657c4e319e3318
hvdv99 <huubvandevoort@home.nl>
2023-10-13 10:34:06 +0000

    data uploaded

--- synchronizer/upload_training_data.bash
+++ synchronizer/upload_training_data.bash
@@ -1,8 +1,8 @@
 gsutil cp ../lab4/data/* gs://data_de2023
 
-git config --global user.email "indika@example.com"
-git config --global user.name "Indika Kumara"
+git config --global user.email "huubvandevoort@home.nl"
+git config --global user.name "hvdv99"
 echo "data uploaded" >> ct_pipeline/history.txt
 git commit -am "data uploaded"
-git push https://$1:$2@github.com/IndikaKuma/DE2023.git --all
+git push https://$1:$2@github.com/hvdv99/DE2023.git --all
 
